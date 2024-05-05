33d95b418aab5741eb16d5e64ec2209f28cef5b6
mateus-gomes-barros <mateusgomesbarros@hotmail.com>
2024-05-05 19:01:52 -0300

    countingVotes

--- urna/script.js
+++ urna/script.js
@@ -9,6 +9,7 @@ let numeros = document.querySelector('.d-1-3');
 let etapaAtual = 0;
 let numero = '';
 let votoBranco = false;
+let votos = [];
 
 function comecarEtapa(){
     let etapa = etapas[etapaAtual];
@@ -109,17 +110,25 @@ function confirma(){
     let votoConfirmado = false;
     if(votoBranco === true){
         votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: 'branco'
+        });
         console.log("Confirmando como Branco");
     } else if(numero.length === etapa.numeros){
         votoConfirmado = true;
-        console.log("Confirmando como "+numero);
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        });
     }
     if(votoConfirmado){
         etapaAtual++;
         if(etapas[etapaAtual] !== undefined){
             comecarEtapa();
         }else{
-            console.log("FIM!!")
+            document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca">FIM</div>';
+            console.log(votos);
         }
     }
 }
